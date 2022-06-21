f80eebd5a82710780719ac53fd1b7b1287dc3099
NTOC23 <omarsalazar.f4@gmail.com>
2022-06-21 08:38:32 -0500

    first day

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let res = 0;
+  let pos = 0;
 
+  for ( let i = num.length - 1; i >= 0; i-- ) {
+    res += num[i] * (2 ** pos);
+    pos++;
+  }
+  return res;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let resto = '';
+  if (num === 0) {
+    resto = '0';
+  }
+  while (num > 0) {
+    resto = num % 2 + resto; 
+    num = Math.floor(num / 2); 
+  }
+  return resto;
 }
 
 
